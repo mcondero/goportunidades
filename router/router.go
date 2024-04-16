3b40439ad764c8aaccd0db85729cf5e396fd97dd
Maurício "Conde" Ramon <mcondero@hotmail.com>
2024-04-16 13:05:41 -0300

    feat: implement routes logic

--- router/router.go
+++ router/router.go
@@ -2,13 +2,15 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/render"
 )
 
 func Initialize() {
+	// Initialize router
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.Render(200, render.JSON{Data: any(gin.H{"message": "pong"})})
-	})
+
+	// Initialize routes
+	initializeRoutes(r)
+
+	// Run server
 	r.Run(":8080")
 }
