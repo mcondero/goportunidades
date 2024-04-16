1c7f9af4a608070f184ecc49b14bd92ef91a9b22
Maurício "Conde" Ramon <mcondero@hotmail.com>
2024-04-16 16:34:38 -0300

    feat: add database handler

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/mcondero/goportunidades/router"
+import (
+	"github.com/mcondero/goportunidades/config"
+	"github.com/mcondero/goportunidades/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	// Initialize Configs
+	logger = *config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("[ERROR] config initialization error: %v", err)
+		return
+	}
+	// Initialize Router
 	router.Initialize()
 }
